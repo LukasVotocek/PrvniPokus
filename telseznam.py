8520d736d32ad62261f05b1704a85becf9c78c07
Lukas Votocek <votocekl@arcig.cz>
2024-12-02 13:56:38 +0100

    fin

---
+++ telseznam.py
@@ -0,0 +1,12 @@
+telefonni_seznam={}
+telefonni_seznam['789456123']='Jan Novak'
+telefonni_seznam['987456321']='Josef Kral'
+telefonni_seznam['654546132']='Eva Pokorna'
+telefonni_seznam['123456789']='Filip Cech'
+telefonni_seznam['778523654']='Anna Vesela'
+t=telefonni_seznam.get('789456123')
+
+
+#class Tel:
+#    def __init__(self,):
+
