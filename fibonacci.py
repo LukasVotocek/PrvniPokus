cbb1cf8b9cd71f855eb253f597aea8f1bf905170
Lukas Votocek <votocekl@arcig.cz>
2024-12-16 15:25:58 +0100

    fib2

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,31 @@
+
+
+#optimalizace s cache
+"""cache = {0:0, 1:1}
+
 def fibonacci(n):
-    if n==0:
-        return 0
-    elif n==1:
-        return 1
-    else:
-        return fibonacci(n-1)+fibonacci(n-2)
+    print(f"pocitam fib z {n}")
+    if n in cache:
+        return cache[n]
+    vysledek = fibonacci(n-1) + fibonacci(n-2)
+    cache[n] = vysledek
+    return vysledek
 
+print(fibonacci(10))"""
 
-print(fibonacci(10))
+#optimalizace se seznamem
+
+def fib(n):
+    if n<=1:
+        return n
+    
+    a=0
+    b=1
+    acc=1
+    while acc<n:
+        c=a+b
+        a=b
+        b=c
+        acc+=1
+    return c
+    
