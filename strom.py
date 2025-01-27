f726e047ef442b455bde1a53e007544463d383fb
Lukas Votocek <votocekl@arcig.cz>
2025-01-27 15:23:05 +0100

    strom3

--- strom.py
+++ strom.py
@@ -1,9 +1,19 @@
 from __future__ import annotations
 from dataclasses import dataclass
+from typing import Optional
 
 @dataclass 
 class Uzel:
-    operace: str
-    levy: Uzel
-    pravy: Uzel
+    hodnota: str
+    levy: Uzel | None = None
+    pravy: Optional[Uzel] = None
 
+
+#(3+2)*4-8
+tri=Uzel("3")
+dva=Uzel("2")
+plus=Uzel("+",tri,dva)
+ctyri=Uzel("4")
+krat=Uzel("*", plus, ctyri)
+osm=Uzel("8")
+minus=Uzel("-",krat,osm)
