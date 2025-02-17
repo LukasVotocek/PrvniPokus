a3010889b2b38616ff7426de17b66426ab20af30
Lukas Votocek <votocekl@arcig.cz>
2025-02-17 15:18:13 +0100

    hledanidosirky

--- strom.py
+++ strom.py
@@ -1,6 +1,11 @@
-"""from __future__ import annotations
+from __future__ import annotations
 from dataclasses import dataclass
-from typing import Optional
+from typing import Optional, Any
+from enum import Enum
+from abc import ABC, abstractmethod
+from collections import deque
+
+"""
 
 @dataclass 
 class Uzel:
@@ -32,12 +37,18 @@ tisk(minus)"""
 #fronta, FIFO (first in, first out)
 #tiskova fronta
 
-from collections import deque
+
+"""
+@dataclass
+class Uloha:
+    file:str
+    user:str
+
 class Printer:
     def __init__ (self) ->None:
         self.fronta=deque()
     def enqueue(self,file,user):
-        self.fronta.append((file,user))    
+        self.fronta.append(Uloha(file,user))    
 
     def print_next(self):
         print(self.fronta.popleft())
@@ -51,4 +62,89 @@ printer.enqueue("tabulka.xls", "Karel")
 printer.enqueue("referat.docx", "Lida")
 printer.enqueue("dovolena.jpeg", "Rudolf")
 
-printer.print_all()
+printer.print_all()"""
+
+
+
+
+
+
+    
+
+class Op(Enum):
+    PLUS = "+"
+    MINUS = "-"
+    TIMES = "*"
+    DIVIDE = "/"
+    
+    def __str__(self) -> str:
+        return self.value
+
+
+class Uzel(ABC):
+    
+    @abstractmethod
+    def eval(self) -> float:
+        pass
+
+
+class ListUzel:
+
+    def __init__(self, hodnota) -> None:
+        self.hodnota = hodnota
+    
+    def __str__(self) -> str:
+        return str(self.hodnota)
+    
+    def eval(self) -> float:
+        return self.hodnota
+        
+
+class BinUzel:
+
+    def __init__(self, operace, levy, pravy) -> None:
+        self.operace = operace
+        self.levy = levy
+        self.pravy = pravy
+
+    def __str__(self) -> str:
+        return str(self.operace)
+    
+    def eval(self) -> float:
+        l = self.levy.eval()
+        p = self.pravy.eval()
+        if self.operace == Op.PLUS:
+            return l + p
+        elif self.operace == Op.MINUS:
+            return l - p
+        elif self.operace == Op.TIMES:
+            return l * p
+        elif self.operace == Op.DIVIDE:
+            return l / p
+        else:
+            raise ValueError(f"Neznama operace {self.operace}?")
+        
+def bfs(koren):
+    fronta=deque()
+    seznam=[]
+    fronta.append(koren)
+    while fronta:
+        celo=fronta.popleft()
+        seznam.append(celo)
+        if isinstance(celo,BinUzel):
+            fronta.append(celo.levy)
+            fronta.append(celo.pravy)
+    return seznam
+
+
+
+uzel10 = ListUzel(10)
+uzel5 = ListUzel(5)
+
+uzel_plus = BinUzel(Op.PLUS, uzel10, uzel5)
+lst=bfs(uzel_plus)
+
+for i in lst:
+    print (i)
+
+print(uzel_plus.eval())
