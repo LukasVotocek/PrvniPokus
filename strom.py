2dc485b22d41e4731e13b6716749e0af6d3064f5
Lukas Votocek <votocekl@arcig.cz>
2025-02-17 14:40:06 +0100

    fronta

--- strom.py
+++ strom.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+"""from __future__ import annotations
 from dataclasses import dataclass
 from typing import Optional
 
@@ -17,3 +17,38 @@ ctyri=Uzel("4")
 krat=Uzel("*", plus, ctyri)
 osm=Uzel("8")
 minus=Uzel("-",krat,osm)
+
+
+def tisk(n:Uzel):
+    if (n.levy is None):
+        print(n.hodnota)
+    else: 
+        tisk(n.levy)
+        print(n.hodnota)
+        tisk(n.pravy)
+
+tisk(minus)"""
+
+#fronta, FIFO (first in, first out)
+#tiskova fronta
+
+from collections import deque
+class Printer:
+    def __init__ (self) ->None:
+        self.fronta=deque()
+    def enqueue(self,file,user):
+        self.fronta.append((file,user))    
+
+    def print_next(self):
+        print(self.fronta.popleft())
+
+    def print_all(self):
+        while self.fronta:
+            self.print_next()
+
+printer = Printer()
+printer.enqueue("tabulka.xls", "Karel")
+printer.enqueue("referat.docx", "Lida")
+printer.enqueue("dovolena.jpeg", "Rudolf")
+
+printer.print_all()
